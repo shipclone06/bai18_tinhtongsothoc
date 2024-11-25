3d407dc4e83cca9b1f18a4eb7d8e91502219ba26
shipclone06 <anhkhoa030900@gmail.com>
2024-11-25 10:32:48 +0700

    Update bai18_tinhtongsothoc.cpp
    update'1

--- bai18_tinhtongsothoc/bai18_tinhtongsothoc.cpp
+++ bai18_tinhtongsothoc/bai18_tinhtongsothoc.cpp
@@ -1,17 +1,26 @@
-#include <cmath>
+// bai_18Tinhsothoctrenbanco.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 int main() {
-	double o, tongthoc = 0, tong = 0;
-	cout << " tinh so thoc\n";
-	cout << "nhap so o: "; cin >> o; cout << endl;
-
-	for (int i = 1; i <= o; i++)
-	{
-		tongthoc = pow(2, i - 1);
-		tong += tongthoc;
-		cout << "o thu " << i << " :" << tongthoc << endl;
+	int SoO, sothoc;
+	cout << "nhap so O : \n"; cin >> SoO;
+	cout.imbue(locale("en_US.UFT-8")); 
+	double tongsothoc = 0;
+	sothoc = 0;
+	if (SoO > 64) {
+		cout << "tren ban co chi co 64 O , vui long nhap so <=64\n";
+		system("pause");
+		return 0;
 	}
-	cout << "tong so thoc dang co:" << tong << endl;
+	for (int i = 0; i < SoO; i++)
+	{
+		tongsothoc = tongsothoc + pow(2, i);
+		cout << "othu" << i << ":" << tongsothoc << endl;
+	}
+	cout << " tong so thoc tren ban co: " << tongsothoc << endl;
+	cout << "tong so thoc tren ban co:" << fixed << setprecision(0) << tongsothoc << endl;
 	return 0;
 }
